461406fa6e9f0e73228c75ee41abfb82ec580f1d
Twirl1984 <christoph.funda@gmail.com>
2024-07-09 19:47:43 +0000

    challenge 5_1 gelöst

--- Challegne2/main.c
+++ Challegne2/main.c
@@ -3,6 +3,10 @@
 // Das ist ein fehlerhaftes Programm, was dieser Kommentar schon deutlich machen soll
 int main()
 {
-    printf("Hello world!\n");
+    printf("Hello world! - n: \n");
+    printf("Hello world!- b: \b");
+    printf("Hello world!- t: \t");
+    printf("Hello world!- v: \v");
+    printf("Hello world!- a: \a");
     return 0;
 }
