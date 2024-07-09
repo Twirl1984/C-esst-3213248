ea2090587b9fc28d4254c08e0d372fe7e79fa4a9
Twirl1984 <christoph.funda@gmail.com>
2024-07-09 19:54:59 +0000

    challenge 5_2 gelöst

--- Challenge5_2/main.c
+++ Challenge5_2/main.c
@@ -5,5 +5,7 @@ int main()
 {
     char id;
     printf("Bitte geben Sie ein einzelnes Zeichen als Zugangscode ein:\n");
+    scanf("%c",&id);
+    (id>'p')? printf("Sie haben Zugang") : printf("ID falsch");
     return 0;
 }
